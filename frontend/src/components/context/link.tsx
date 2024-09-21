5e9c45c8377f0753a841b22b90c1bff5a482ce15
adavidarevalo <davidarevaloc20@gmail.com>
2024-09-21 17:13:47 -0600

    feat: add view state management to LinksContext and update LinksProvider to support view switching

--- frontend/src/components/context/link.tsx
+++ frontend/src/components/context/link.tsx
@@ -1,7 +1,17 @@
-import { yupResolver } from '@hookform/resolvers/yup';
-import React, { createContext, ReactNode, useContext } from 'react';
-import { Control, FieldArrayWithId, FieldErrors, useFieldArray, UseFieldArrayAppend, UseFieldArrayMove, UseFieldArrayRemove, useForm, UseFormHandleSubmit } from 'react-hook-form';
-import { linkFormSchema } from '../schemas/link';
+import { yupResolver } from "@hookform/resolvers/yup";
+import React, { createContext, ReactNode, useContext, useState } from "react";
+import {
+  Control,
+  FieldArrayWithId,
+  FieldErrors,
+  useFieldArray,
+  UseFieldArrayAppend,
+  UseFieldArrayMove,
+  UseFieldArrayRemove,
+  useForm,
+  UseFormHandleSubmit,
+} from "react-hook-form";
+import { linkFormSchema } from "../schemas/link";
 
 interface Link {
   platform: string;
@@ -25,13 +35,15 @@ const initialState: LinksState = {
 };
 
 const LinksContext = createContext<{
-  control: Control<LinksState, any> | null
-  errors: FieldErrors<LinksState>
-  handleSubmit: UseFormHandleSubmit<LinksState, undefined> | null
-  move: UseFieldArrayMove
-  remove: UseFieldArrayRemove
-  append: UseFieldArrayAppend<LinksState, "links">
-  fields: FieldArrayWithId<LinksState, "links", "id">[]
+  control: Control<LinksState, any> | null;
+  errors: FieldErrors<LinksState>;
+  handleSubmit: UseFormHandleSubmit<LinksState, undefined> | null;
+  move: UseFieldArrayMove;
+  remove: UseFieldArrayRemove;
+  append: UseFieldArrayAppend<LinksState, "links">;
+  fields: FieldArrayWithId<LinksState, "links", "id">[];
+  setView: React.Dispatch<React.SetStateAction<"links" | "profile">>;
+  view: "links" | "profile";
 }>({
   control: null,
   errors: {},
@@ -40,10 +52,12 @@ const LinksContext = createContext<{
   remove: () => {},
   append: () => {},
   fields: [],
+  setView: () => {},
+  view: "links",
 });
 
-
 export const LinksProvider = ({ children }: { children: ReactNode }) => {
+  const [view, setView] = useState<"links" | "profile">("links");
 
   const {
     control,
@@ -57,10 +71,22 @@ export const LinksProvider = ({ children }: { children: ReactNode }) => {
   const { fields, append, remove, move } = useFieldArray({
     control: control!,
     name: "links",
-  });  
+  });
 
   return (
-    <LinksContext.Provider value={{ control, errors, handleSubmit, fields, append, remove, move }}>
+    <LinksContext.Provider
+      value={{
+        control,
+        errors,
+        handleSubmit,
+        fields,
+        append,
+        remove,
+        move,
+        view,
+        setView,
+      }}
+    >
       {children}
     </LinksContext.Provider>
   );
