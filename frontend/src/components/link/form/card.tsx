e91f54f653778cab21b2721d6973b3fbaf579b52
adavidarevalo <davidarevaloc20@gmail.com>
2024-09-21 12:48:16 -0600

    refactor: simplify Card component by using context for control and error handling

--- frontend/src/components/link/form/card.tsx
+++ frontend/src/components/link/form/card.tsx
@@ -1,32 +1,23 @@
-import React from "react";
 import { Draggable } from "react-beautiful-dnd";
 import {
-  Control,
   Controller,
   FieldArrayWithId,
-  FieldErrors,
-  UseFieldArrayRemove,
 } from "react-hook-form";
 import { FormValues } from ".";
-import { Box, MenuItem, Select, Typography, FormHelperText } from "@mui/material";
+import { Box, MenuItem, Select, Typography } from "@mui/material";
 import FormInput from "../../form/input";
 import { IoIosArrowDown } from "react-icons/io";
 import { platforms } from "../../../utils/const/plataforms";
+import _ from "lodash";
+import { useLinks } from "../../context/link";
 
 interface CardProps {
   field: FieldArrayWithId<FormValues, "links", "id">;
   index: number;
-  remove: UseFieldArrayRemove;
-  control: Control<FormValues, any>;
-  errors: FieldErrors<FormValues>;
 }
 
-export default function Card({
-  control,
-  field,
-  index,
-  remove,
-}: CardProps) {
+export default function Card({ field, index }: CardProps) {
+  const { control, errors, remove } = useLinks();
   return (
     <Draggable key={field.id} draggableId={field.id} index={index}>
       {(provided) => (
@@ -39,66 +30,74 @@ export default function Card({
             padding: "16px",
             borderRadius: "8px",
             marginBottom: "16px",
-            backgroundColor: "#fff",
+            backgroundColor: "#FAFAFA",
           }}
         >
           <Box display="flex" justifyContent="space-between" mb={"10px"}>
-            <Typography variant="body1" fontWeight={600} color={"#737373"} fontSize={"1rem"}>= Link #{index + 1}</Typography>
-            <Typography onClick={() => remove(index)} variant="body1" sx={{ cursor: 'pointer', '&:hover': { color: '#633cff' } }}>
+            <Typography
+              variant="body1"
+              fontWeight={600}
+              color={"#737373"}
+              fontSize={"1rem"}
+            >
+              = Link #{index + 1}
+            </Typography>
+            <Typography
+              onClick={() => remove(index)}
+              variant="body1"
+              sx={{ cursor: "pointer", "&:hover": { color: "#633cff" } }}
+            >
               Remove
             </Typography>
           </Box>
 
           <Controller
             name={`links.${index}.platform`}
-            control={control}
+            control={control!}
             render={({ field }) => (
-              <div>
-                <Select
-                  {...field}
-                  fullWidth
-                  displayEmpty
-                  IconComponent={IoIosArrowDown} // Use your custom icon here
-                  style={{
-                    display: "flex !important", 
-                    alignItems: "center", 
-                  }}
-                  sx={{
-                    textAlign: 'justify', 
-                    '& .MuiSelect-icon': { color: '#633cff' }, 
-                    border: '1px solid #e0e0e0', 
-                    '&:hover': {
-                      borderColor: '#633cff', 
-                      boxShadow: '0 0 32px 0 rgba(99,60,255,0.25)', 
-                    },
-                    transition: 'border-color 0.3s ease, box-shadow 0.3s ease'
-                  }} // Arrow icon color, border color, box shadow, and transition
-                >
-                  <MenuItem value="" disabled>
-                    Select Platform
+              <Select
+                {...field}
+                fullWidth
+                displayEmpty
+                IconComponent={IoIosArrowDown}
+                sx={{
+                  display: "flex !important",
+                  alignItems: "center",
+                  textAlign: "justify",
+                  "& .MuiSelect-icon": { color: "#633cff" },
+                  border: "1px solid #e0e0e0",
+                  "&:hover": {
+                    borderColor: "#633cff",
+                    boxShadow: "0 0 32px 0 rgba(99,60,255,0.25)",
+                  },
+                  transition: "border-color 0.3s ease, box-shadow 0.3s ease",
+                }}
+              >
+                {platforms.map((platform) => (
+                  <MenuItem key={platform.name} value={platform.name}>
+                    <Box style={{ marginRight: "5px" }}>
+                      {<platform.icon color={"#737373"}/>}
+                    </Box>
+                    {platform.name}
                   </MenuItem>
-                  {platforms.map((platform) => (
-                    <MenuItem key={platform.name} value={platform.name}>
-                      <Box style={{marginRight: "5px"}}>
-                      {<platform.icon/>}
-                        </Box> {platform.name}
-                    </MenuItem>
-                  ))}
-                </Select>
-                {errors.links && errors.links[index]?.platform && (
-                  <FormHelperText error>
-                    {errors.links[index]?.platform?.message}
-                  </FormHelperText>
-                )}
-              </div>
+                ))}
+              </Select>
             )}
           />
 
+          {_.get(errors, `links.[${index}].platform`) && (
+            <Typography variant="body2" color="error">
+              {_.get(errors, `links.[${index}].platform.message`)}
+            </Typography>
+          )}
+
           <FormInput
-            name={`links.${index}.link`}
+            name={`links[${index}].link`}
             label={"Link"}
             type="string"
+            control={control}
             placeholder="e.g. https://github.com/johndoe"
+            errors={errors}
           />
         </Box>
       )}
